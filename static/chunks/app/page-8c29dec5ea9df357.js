2ee0e92f9c2c42cea7388364f2a044ad0703e810
Victor <omondikochich@gmail.com>
2024-11-01 21:18:05 +0300

    Uploaded NPM Build

---
+++ static/chunks/app/page-8c29dec5ea9df357.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,286],{5734:function(n,e,t){Promise.resolve().then(t.t.bind(t,6018,23)),Promise.resolve().then(t.t.bind(t,2972,23))},6018:function(){}},function(n){n.O(0,[418,972,971,117,744],function(){return n(n.s=5734)}),_N_E=n.O()}]);
